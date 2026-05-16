9e3a45e575fefc47767f2ca8c5202aa84541cb46
MisfitMEdiAhouse <pricemedia82@gmail.com>
2026-05-15 18:02:01 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        nexnavy: "#071827",
+        nexslate: "#0B1220",
+        nexteal: "#14B8A6",
+        nexgreen: "#22C55E",
+        nexamber: "#F59E0B"
+      }
+    }
+  },
+  plugins: []
+};
